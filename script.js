511650702929ea6b7f6fe2beb330e8f5216e024b
nikhil bagal <nbagal02@gmail.com>
2019-10-13 01:21:02 +0530

    12hr and 24hr time is added

--- script.js
+++ script.js
@@ -3,14 +3,34 @@ function init() {
     tick();
 }
 function tick() {
-  //  let element = document.querySelector('#hour');
+    let selection = document.querySelectorAll("input");
+    let element;
     let d = new Date();
-   // element.innerHTML = d.getHours()
+    //2018, 11, 24, 0, 33, 30, 0;
     let hour = document.querySelector('#hour');
-    hour.innerHTML = d.getHours()-12;
     let minute = document.querySelector('#minute');
-    minute.innerHTML = d.getMinutes();
     let second = document.querySelector('#second');
-    second.innerHTML = d.getSeconds();
+    for(var i=0;i<selection.length;i++){
+        if(selection[i].checked){
+            element = selection[i].value;
+        }
+    }
+    if(element === "12Hr"){
+        if(d.getHours()>12){
+        hour.innerHTML = d.getHours()-12;
+        }
+        else if(d.getHours()==0){
+        hour.innerHTML = d.getHours()+12;
+        }
+        else {
+        hour.innerHTML = d.getHours();
+        }
+        minute.innerHTML = d.getMinutes();
+        second.innerHTML = d.getSeconds();
+    }else if(element === "24Hr"){
+        hour.innerHTML = d.getHours();
+        minute.innerHTML = d.getMinutes();
+        second.innerHTML = d.getSeconds();
+    }  
 }
 setInterval(tick,1000);
